562a0803bf24b95db9364954c4345ade565744eb
DereckAn <dereckangeles15071998@gmail.com>
2023-05-27 04:54:38 -0600

    hasta cambiar colores y palabra

--- color_game/lib/components/menu.dart
+++ color_game/lib/components/menu.dart
@@ -5,24 +5,71 @@ class Menu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final widtt = MediaQuery.of(context).size.width;
+    final highh = MediaQuery.of(context).size.height;
+
     return Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Center(
-              child: Text(
-                'Color Matcher Game', style: TextStyle(fontSize: 50),
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        // Text(
+        //   'Color Matcher Game $widtt',
+        //   style: TextStyle(fontSize: 40),
+        // ),
+        Image.asset(
+          'assets/images/pato.png',
+          width: 200,
+          fit: BoxFit.fill,
+        ),
+        SizedBox(
+          height: highh / 10,
+        ),
+        SizedBox(
+          width: widtt / 1.5,
+          height: highh / 17,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 66, 190, 165),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
               ),
             ),
-            TextButton(onPressed: (){
+            onPressed: () {
               Navigator.pushNamed(context, '/juego');
-            }, 
-            child: const Text('START'),),
-            TextButton(onPressed: (){
+            },
+            child: const Text(
+              'START',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w900,
+                  fontSize: 16),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: highh / 60,
+        ),
+        SizedBox(
+          width: widtt / 1.5,
+          height: highh / 17,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+            onPressed: () {
               Navigator.pushNamed(context, '/juego');
-            }, 
-            child: const Text('STATS'),),
-
-          ],
-        );
+            },
+            child: const Text(
+              'Records',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 66, 190, 165),
+                  fontWeight: FontWeight.w900,
+                  fontSize: 16),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
--- color_game/lib/screens/playing.dart
+++ color_game/lib/screens/playing.dart
@@ -1,4 +1,36 @@
+import 'package:color_game/components/two_buttons.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+final colors = [
+  Colors.red,
+  Colors.pink,
+  Colors.purple,
+  Colors.blue,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.brown,
+  Colors.grey,
+  Colors.black,
+  Colors.white,
+];
+
+final colors2 = [
+  'red',
+  'pink',
+  'purple',
+  'blue',
+  'green',
+  'yellow',
+  'orange',
+  'brown',
+  'grey',
+  'black',
+  'white',
+];
+
+final random = Random();
 
 class Playing extends StatefulWidget {
   const Playing({super.key});
@@ -8,8 +40,47 @@ class Playing extends StatefulWidget {
 }
 
 class _PlayingState extends State<Playing> {
+  void _changeBackgroundColor() {
+    final randomColor = colors[random.nextInt(colors.length)];
+    final stringColor = colors2[random.nextInt(colors2.length)];
+    setState(() {
+      _backgroundColor = randomColor;
+      _stringColor = stringColor;
+    });
+  }
+
+  Color _backgroundColor = Colors.pink;
+  String _stringColor = 'Ready?';
+
   @override
   Widget build(BuildContext context) {
-    return const Text("Aqui empezaras a jugar perro ");
+    return Scaffold(
+      backgroundColor: _backgroundColor,
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: const Text(
+          'Playing',
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Expanded(
+              flex: 2,
+              child: Center(
+                  child: Text(
+                _stringColor,
+                style: const TextStyle(fontSize: 50),
+              ))),
+          Expanded(flex: 2, child: TwoButtons(_changeBackgroundColor)),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
+    );
   }
 }
