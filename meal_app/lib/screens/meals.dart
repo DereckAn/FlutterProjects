a11ab565242cd4909c13eaa59206c85fc67e295e
DereckAn <dereckangeles15071998@gmail.com>
2023-07-11 08:48:21 -0600

    creando listview

--- meal_app/lib/screens/meals.dart
+++ meal_app/lib/screens/meals.dart
@@ -9,11 +9,29 @@ class MealsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+      itemCount: meals.length,
+      itemBuilder: (context, index) {
+      return ListTile(
+        title: Text(meals[index].title),
+      );
+    });
+    if (meals.isEmpty) {
+      content =  Center(
+        child: Column(
+          children: [
+            Text('Uh oh ... nothing here!', style: Theme.of(context).textTheme.headlineLarge!.copyWith(color: Theme.of(context).colorScheme.onBackground,),),
+            const SizedBox(height: 20),
+            Text('Add some meals to get started or change category', style: Theme.of(context).textTheme.bodyLarge!.copyWith(color: Theme.of(context).colorScheme.onBackground,),),
+          ],
+        ),
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: ,
+      body: content,
     );
   }
 }
