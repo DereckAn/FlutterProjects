6be229cc4487ae9f74c1b8979e17c218dd578cc1
DereckAn <dereckangeles15071998@gmail.com>
2023-08-08 15:28:35 -0600

    feat: ading a drawer in filterScreen

--- meal_app/lib/screens/filter.dart
+++ meal_app/lib/screens/filter.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/screens/tabs.dart';
+import 'package:meal_app/widgets/main_drawer.dart';
 
 class FilterScreen extends StatefulWidget {
   const FilterScreen({super.key});
@@ -14,27 +16,43 @@ class _FilterScreenState extends State<FilterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('filters'),
+        title: const Text('Filters'),
       ),
+      // drawer: MainDrawer(
+      //   onSelectScreen: (iden) {
+      //     Navigator.of(context).pop();
+      //     if (iden == 'meals') {
+      //       Navigator.of(context).pushReplacement(
+      //         MaterialPageRoute(
+      //           builder: (context) => const TabsScreen(),
+      //         ),
+      //       );
+      //     }
+      //   },
+      // ),
       body: Column(
         children: [
           SwitchListTile(
             value: _gluttenFree,
-            onChanged: (ischecked){
+            onChanged: (ischecked) {
               setState(() {
                 _gluttenFree = ischecked;
               });
             },
-            title: Text('Gluten-free', style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            title: Text(
+              'Gluten-free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
                     color: Theme.of(context).colorScheme.onBackground,
-                  ),),
-            subtitle:  Text(
-              'Only include gluten-free meals', style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                  ),
+            ),
+            subtitle: Text(
+              'Only include gluten-free meals',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
                     color: Theme.of(context).colorScheme.onBackground,
                   ),
             ),
             activeColor: Theme.of(context).colorScheme.tertiary,
-            contentPadding: const EdgeInsets.only(left: 34,right: 22),
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
           ),
         ],
       ),
