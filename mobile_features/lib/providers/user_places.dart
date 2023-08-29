b1e356ad9e590201e7c9da4969ab0c009891a3f5
Dereck <dereckangeles15071998@gmail.com>
2023-08-29 02:14:24 -0600

    adding dependencies and starting to save things on the device

--- mobile_features/lib/providers/user_places.dart
+++ mobile_features/lib/providers/user_places.dart
@@ -2,16 +2,27 @@ import 'dart:io';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mobile_features/models/place.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
 
 class UserPLacesNotifier extends StateNotifier<List<Place>> {
   UserPLacesNotifier() : super(const []); // this is the constructor
 
   void add(String title, File image, 
   // PlaceLocation location
-  ) {
+  ) async {
+
+    // crear el path donde guardaremos la foto 
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    
+    final fileName = path.basename(image.path);
+
+    final copiedImage = await image.copy('${appDir.path}/$fileName');
+
+
     // void add(Place place)
     // state = [...state, place];
-    final newPlace = Place(title: title, image: image, );
+    final newPlace = Place(title: title, image: copiedImage, );
     state = [newPlace, ...state ]; // En este orden siemre el titulo nuevo estara al principio de la lista 
   }
 
