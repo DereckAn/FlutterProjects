24781023368000c0dadccdc17c0f33266365b23d
Dereck <dereckangeles15071998@gmail.com>
2023-10-04 22:11:00 -0600

    MUY IMPORTANTE: mandando mensajes y guardandolos en firebase

--- chat_app/lib/widgets/new_message.dart
+++ chat_app/lib/widgets/new_message.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class NewMessage extends StatefulWidget {
   const NewMessage({super.key});
@@ -8,34 +10,59 @@ class NewMessage extends StatefulWidget {
 }
 
 class _NewMessageState extends State<NewMessage> {
-  var messageController = TextEditingController();
+  //Esta es la variable que almacenara los datos a mandar a firebase
+  final messageController = TextEditingController();
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
     //Recuerda hacerle dispose al controlador para que no se quede en memoria y no se llene
     messageController.dispose();
   }
 
-  void submitMessage(){
+  void submitMessage() async {
+    
     final message = messageController.text;
-    if(message.trim().isEmpty){
+    if (message.trim().isEmpty) {
       return;
     }
-    //Aqui se envia el mensaje
 
-    //To reset the input field
+     //To reset the input field
+    FocusScope.of(context).unfocus(); // Esto es ara quitar el teclado despues de mandar un mensaje
     messageController.clear();
-  }
 
+    //Cuidado con FirebaseAuth y FirebaseFirestore. Dos cosas totalmente diferentes
+    final user = FirebaseAuth.instance.currentUser!;
+
+    final userImages = await FirebaseFirestore
+        .instance //Aqui estamos obteniendo la instancia de firebase
+        .collection('users') // Aqui estamos obteniendo la coleccion de usuarios
+        .doc(user.uid) // Aqui estamos obteniendo el id del usuario
+        .get(); //Esto mando un http request para obtener la informacion
+    // .then((value) => value.data()!['image_url']);
+
+    //Aqui se envia el mensaje
+    FirebaseFirestore.instance.collection('chat').add({
+      'text': message,
+      'createdAt': Timestamp.now(),
+      'userId': user.uid,
+      //Recuerda que para tener esta informacion tenemos que jalarla de firebase
+      //Aqui estamos accediendo a la tabla de datos en la seccion de username e image_url
+      'username': userImages.data()!['username'],
+      'userImage': userImages.data()!['image_url'],
+    });
+
+   
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 15, right: 1, bottom: 14),
       child: Row(children: [
-         Expanded(child: TextField(
+        Expanded(
+            child: TextField(
           controller: messageController,
           decoration: const InputDecoration(labelText: 'Send a message...'),
           textCapitalization: TextCapitalization.sentences,
