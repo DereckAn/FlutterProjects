9e24740f687952b786ce4c83359ef6e81735e218
DereckAn <dereckangeles15071998@gmail.com>
2023-10-06 22:58:31 -0600

    IMPORTANTE: agregando burbuja de usuarios para mostrar sus mensajes

--- chat_app/lib/widgets/chat_messages.dart
+++ chat_app/lib/widgets/chat_messages.dart
@@ -1,4 +1,6 @@
+import 'package:chat_app/widgets/message_bubble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -6,12 +8,17 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authenticatedUser = FirebaseAuth.instance.currentUser!;
+
     return StreamBuilder(
       // Estamos accediendo a la collecion que esta en la instancia de firebasefirestore. Y la estamos convirtiendo en un stream
       // Para eso usamos el --> .snapshots()
       // Aqui estamos ordenando como queremos los datos --> .orderBy()
       // Estamos viendo si hay cambios en esa coleccion
-      stream: FirebaseFirestore.instance.collection('chat').orderBy('createdAt', descending: false).snapshots(),
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: true)
+          .snapshots(),
       builder: (ctx, AsyncSnapshot<QuerySnapshot> chatSnapshot) {
         //aqui estamos haceidno una condicion que si la coneccion esta esperando
         //Mostraremos el circulo de carga
@@ -27,26 +34,71 @@ class ChatMessages extends StatelessWidget {
           );
         }
 
-
-        // Esto es para chechar si huubo un error en la captura de datos. 
+        // Esto es para chechar si huubo un error en la captura de datos.
         if (chatSnapshot.hasError) {
           return const Center(
             child: Text('Something went wrong'),
           );
         }
 
-        //esto es para calcular los datos. En una lista 
+        //esto es para calcular los datos. En una lista
         final chatDocs = chatSnapshot.data!.docs;
 
         // Aqui vamos a mostrar los mensaes como un listview.
         // Generandolos dependiendo el usuario va bajando.
         return ListView.builder(
+          padding: const EdgeInsets.only(bottom: 40, left: 13, right: 13),
+
+          // Este parametro hace que podamos ver los mensajes de abajo hacia arriba.
+          // ? Cambia el orden de los mensajes
+          // Tambien debemos cambiar el orden de como llamamos los datos en el stream
+          // & linea 14
+          reverse: true,
           itemCount: chatDocs.length,
 
-          //Aqui estamos modificando como y que se mostrara en la pantalla. 
-          itemBuilder: (ctx, index) => Text(chatDocs[index]['text']),
+          //Aqui estamos modificando como y que se mostrara en la pantalla.
+          itemBuilder: (ctx, index) {
+            // ? Toda esta parte es para checar si el mensaje es el primero usuario o no.
+
+            // NOTE: Aqui estamos accediendo a los datos de la coleccion
+            // ! si ponemos .data() nos dara un error.
+            final chatMessages = chatDocs[index];
+
+            // NOTE: No estoy muy seguro que estamos haciendo aqui.
+            // NOTE Creo que estamos accediendo a los datos de la coleccion
+            // ! si ponemos .data() nos dara un error.
+            final nextChat =
+                index + 1 < chatDocs.length ? chatDocs[index + 1] : null;
+
+            final currentMessageUserID = chatMessages['userId'];
+            final nextMessageUserID =
+                nextChat != null ? nextChat['userId'] : null;
+
+            final nextUserisSame = nextMessageUserID == currentMessageUserID;
+
+            if (nextUserisSame) {
+              return MessageBubble.next(
+                message: chatMessages['text'],
+                isMe: authenticatedUser.uid == currentMessageUserID,
+              );
+            } else {
+              return MessageBubble.first(
+                userImage: chatMessages['userImage'],
+                message: chatMessages['text'],
+                username: chatMessages['username'],
+                isMe: authenticatedUser.uid == currentMessageUserID,
+              );
+            }
+          },
         );
       },
     );
   }
 }
+
+// ?
+// !
+// TODO
+// *
+// NOTE
+// &
